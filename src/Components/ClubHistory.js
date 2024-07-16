e58eaf6a6fa823086ee7883c4a528f90a176197e
Brenton Turnor <Brenton.Turnor@insight.com>
2024-07-16 10:10:11 +0800

    Fixed unused vars

--- src/Components/ClubHistory.js
+++ src/Components/ClubHistory.js
@@ -1,14 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+//import { useState } from 'react';
 import "./ClubHistory.css"
 import * as XLSX from 'xlsx';
 
 export const ClubHistory = () => {
 
-    const [data, setData] = useState([]);
-    const [data2022, set2022Data] = useState([]);
-    const [totalWinnings, setTotalWinnings] = useState([]);
-    const [totalWinnings2022, setTotalWinnings2022] = useState([]);
+    //const [data, setData] = useState([]);
+    //const [data2022, set2022Data] = useState([]);
+    //const [totalWinnings, setTotalWinnings] = useState([]);
+//    const [totalWinnings2022, setTotalWinnings2022] = useState([]);
 
     var url = './Data/HEMANPUNTERS.xlsx';
     var oReq = new XMLHttpRequest();
@@ -48,13 +49,13 @@ export const ClubHistory = () => {
 
             filteredResults2.sort((a, b) => b.__EMPTY_3 - a.__EMPTY_3)
 
-            setData(filteredResults2);
+            //setData(filteredResults2);
 
-            const totalWinnings = parsedData.slice(54, 55)
+            //const totalWinnings = parsedData.slice(54, 55)
 
-            const totalWinnings2 = totalWinnings.map(({ __EMPTY_2, __EMPTY_3 }) => ({ __EMPTY_2, __EMPTY_3 }))
+            //const totalWinnings2 = totalWinnings.map(({ __EMPTY_2, __EMPTY_3 }) => ({ __EMPTY_2, __EMPTY_3 }))
 
-            setTotalWinnings(totalWinnings2)
+            //setTotalWinnings(totalWinnings2)
 
             /* Get 2022 worksheet content*/
             var worksheet2022 = workbook.Sheets[sheet_name_2022];
@@ -72,7 +73,7 @@ export const ClubHistory = () => {
                 element.__EMPTY_3 = element.__EMPTY_3.toFixed(2)
             })
 
-            set2022Data(filteredResults2022);
+            //set2022Data(filteredResults2022);
 
             const totalWinning2022 = parsedData2022.slice(49, 50)
 
@@ -82,7 +83,7 @@ export const ClubHistory = () => {
                 element.__EMPTY_3 = element.__EMPTY_3.toFixed(2)
             })
 
-            setTotalWinnings2022(totalWinnings2022)
+            //setTotalWinnings2022(totalWinnings2022)
 
         }
 
